d62db7c161160478b97efe5fd116210d580a8e16
Jin Choi <jin.choi@enjine.com>
2023-08-08 14:04:03 -0700

    created VolatilityForecastModule

--- main.py
+++ main.py
@@ -1,5 +1,45 @@
-import yfinance as yf
+import datetime
 
-spy = yf.Ticker("SPY")
+import numpy as np
+import pytz as pytz
+import torch
+import yfinance as yf
+from matplotlib import pyplot as plt
+import pandas as pd
+
+from model.trainer import evaluate_model, train_model, predict
+
+# Get the closing price of the S&P 500 Index ETF
+# spy = yf.Ticker("SPY")
+# price_series = spy.history(period='max')['Close']
+#
+# # Calculate the 5-day rolling volatility
+# volatility_series = price_series.pct_change().rolling(5).std().dropna()
+# volatility_series.to_pickle('volatility_series.pkl')
+
+volatility_series = pd.read_pickle('volatility_series.pkl')
+
+# Show that volatility's distribution is heavily skewed to the right
+volatility_series.plot.hist(bins=30)
+plt.show()
+
+# Take the log of the volatility so the distribution looks much more Gaussian
+volatility_series = pd.Series(np.log(volatility_series.values), index=volatility_series.index).dropna()
+volatility_series.plot.hist(bins=30)
+plt.show()
+
+
+# Segment training dataset using cutoff date
+training_cutoff = datetime.datetime(2020, 1, 1, tzinfo=pytz.timezone('America/New_York'))
+
+if torch.cuda.is_available():
+    torch.set_default_tensor_type('torch.cuda.FloatTensor')  # Store all training data in the GPU
+else:
+    torch.set_default_tensor_type('torch.FloatTensor')
+
+training_series = volatility_series.loc[volatility_series.index < training_cutoff]
+results = train_model(training_series, epochs=1)
+
+predict_df = predict(results, volatility_series)
 
 print(spy.info)
---
+++ model/hmm_module.py
@@ -0,0 +1,53 @@
+import pyro
+import pyro.distributions as dist
+import torch
+from pyro.nn import PyroModule
+
+
+class VolatilityForecastModule(PyroModule):
+    def forward(self, x: torch.Tensor, warmup_periods: int = 5):
+        """
+        Uses Kalman filter to forecast volatilities
+        :param x: Volatility data to train the Kalman filter with
+        :param warmup_periods: Number of periods to skip analysis on, to allow the model to "warm up" before making
+        predictions for analysis.
+        """
+        transition_scale = pyro.sample("trans_scale", dist.LogNormal(-2., 3.))
+        observation_scale = pyro.sample("obs_scale", dist.LogNormal(-2., 3.))
+        init_dist_scale = pyro.sample("init_dist_scale", dist.LogNormal(-2., 3.))
+
+        n_t = x.shape[1]
+
+        vol_loc = x[0, 0].unsqueeze(-1).unsqueeze(-1)
+        vol_scale = torch.tensor([[init_dist_scale]]).expand([1, 1, 1])
+
+        transition_matrix = torch.tensor([[1.]])
+        transition_dist = dist.Normal(0., transition_scale.unsqueeze(-1)).to_event(1)
+
+        observation_matrix = torch.tensor([[1.]])
+        observation_dist = dist.Normal(0., observation_scale.unsqueeze(-1)).to_event(1)
+
+        forecasts = []
+        for t in range(n_t):
+            init_dist = dist.MultivariateNormal(vol_loc, vol_scale)
+
+            hmm = dist.GaussianHMM(
+                init_dist,
+                transition_matrix,
+                transition_dist,
+                observation_matrix,
+                observation_dist,
+                duration=1
+            )
+
+            next_x = x[0, t].unsqueeze(-1).unsqueeze(-1)
+            if t >= warmup_periods:
+                pyro.sample(f"obs_{t}", hmm, obs=next_x)
+
+            new_init_dist = hmm.filter(next_x)
+            vol_loc = new_init_dist.loc.detach()
+            vol_scale = new_init_dist.covariance_matrix.detach()
+
+            forecasts.append((vol_loc).squeeze())
+
+        pyro.deterministic(f"predicted_volatility", torch.stack(forecasts, dim=0))
